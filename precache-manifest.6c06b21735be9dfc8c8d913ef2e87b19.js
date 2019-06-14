49ce3134e1057816044538d2a90911fa8b78b0ef
ziming <ziye@ucsc.edu>
2019-06-14 00:08:48 -0700

    Updates

---
+++ precache-manifest.6c06b21735be9dfc8c8d913ef2e87b19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c01f58b5b67cfb71d1456eb3e0164b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bdb33473c071b89b8af",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "beac3e104707a9d6b652",
+    "url": "/static/js/2.e36cc0b2.chunk.js"
+  },
+  {
+    "revision": "5bdb33473c071b89b8af",
+    "url": "/static/js/main.6b039251.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
